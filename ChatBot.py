e87db9174e5a5927d1189592cba0ff170001b68c
max <seemantsinghsanwal@gmail.com>
2021-03-07 03:05:37 +0530

    Error caused due to new line resolved. Emojis removed

--- ChatBot.py
+++ ChatBot.py
@@ -33,23 +33,41 @@ raw = f.read()
 # In[206]:
 
 
-chat_list = raw.lower().split('\n')
-
-
-# In[208]:
-
-
-training_chat_count = int(input("Enter number of chats you want to train with"))
+re_date_time = '\d\d/\d\d/\d\d, \d?\d:\d\d '
+chat_list = re.split(re_date_time, raw.lower())
 
 
 # In[209]:
 
 
-some_chats = chat_list[:training_chat_count]
+some_chats = chat_list[1:]
 
 
 # In[210]:
-
+def remove_emoji_newline(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002500-\U00002BEF"  # chinese char
+                           u"\U00002702-\U000027B0"
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           u"\U0001f926-\U0001f937"
+                           u"\U00010000-\U0010ffff"
+                           u"\u2640-\u2642"
+                           u"\u2600-\u2B55"
+                           u"\u200d"
+                           u"\u23cf"
+                           u"\u23e9"
+                           u"\u231a"
+                           u"\ufe0f"  # dingbats
+                           u"\u3030"
+                           u"\n"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+    
 
 def seperate_received_sent(chat_list, sender, receiver):
     count = 0
