fb33627ac1288f96227d149ecccadbd1bcf4b0dd
max <seemantsinghsanwal@gmail.com>
2021-03-11 19:59:47 +0530

    remove emoji

--- whatsappChatbot/extract_chats.py
+++ whatsappChatbot/extract_chats.py
@@ -1,5 +1,29 @@
 import re
 
+def remove_emoji_newline(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002500-\U00002BEF"  # chinese char
+                           u"\U00002702-\U000027B0"
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           u"\U0001f926-\U0001f937"
+                           u"\U00010000-\U0010ffff"
+                           u"\u2640-\u2642"
+                           u"\u2600-\u2B55"
+                           u"\u200d"
+                           u"\u23cf"
+                           u"\u23e9"
+                           u"\u231a"
+                           u"\ufe0f"  # dingbats
+                           u"\u3030"
+                           u"\n"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
 #seperating chats into list of 2 users
 def extract_chats():
     sender_name = input("Give Sender Name(Keep it sames as in the file):")
@@ -18,11 +42,11 @@ def extract_chats():
         sent_msg = ''
         try:
             while sender_name+':' in chat_list[count]:
-                received_msg += chat_list[count].split(':')[2]
+                received_msg += remove_emoji_newline(chat_list[count].split(':')[1])
                 count += 1
             received.append(received_msg)
             while receiver_name+':' in chat_list[count]:
-                sent_msg += chat_list[count].split(':')[2]
+                sent_msg += remove_emoji_newline(chat_list[count].split(':')[1])
                 count += 1
             sent.append(sent_msg)
         #currently returning value in the except condition as the current logic fails after 
