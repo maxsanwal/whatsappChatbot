1fa789a7fefd70916a86f87d07451991c5a4050b
max <seemantsinghsanwal@gmail.com>
2021-03-11 19:35:11 +0530

    moved code to extract chats and core

--- whatsappChatbot/core.py
+++ whatsappChatbot/core.py
@@ -1,2 +1,9 @@
 from .extract_chats import extract_chats
 
+def 
+sender = input("Give Sender Name(Keep it sames as in the file):")
+receiver = input("Give Your name(Keep in same as in the file):")
+
+received_messages, sent_messages = extract_chats()
+
+
