65a61d0fa71c95f8a80cff9b8276a267a94360fd
max <seemantsinghsanwal@gmail.com>
2021-03-11 21:11:43 +0530

    restructure

---
+++ whatsappChatbot/selenium_automation.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+def automation
